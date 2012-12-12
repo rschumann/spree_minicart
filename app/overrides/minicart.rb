2673ff9b0aa1f81f192aaee15083b54628afa17f
rschumann <sebastians@jevvel.com>
2012-12-12 11:16:01 +0100

    Update app/overrides/minicart.rb

--- app/overrides/minicart.rb
+++ app/overrides/minicart.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => 'spree/shared/_main_nav_bar',
                     :name => 'add_mini_cart',
-                    :insert_bottom => "#link-to-cart",
+                    :insert_bottom => "#navigation",
                     :partial => "spree/shared/minicart")
